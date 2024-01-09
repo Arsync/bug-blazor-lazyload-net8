a13f30dc7949953bb5c06ff139dd949b8afc04a3
Philipp Diethelm <philipp.diethelm@bme-ag.ch>
2024-01-09 10:40:17 +0100

    Test Project .NET7

---
+++ Client/App.razor.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.WebAssembly.Services;
+
+namespace LazyTest.Client
+{
+    public partial class App
+    {
+        [Inject]
+        private LazyAssemblyLoader AssemblyLoader { get; set; } = null!;
+
+        private async Task OnNavigateAsync(NavigationContext args)
+        {
+            if (args.Path == "lazy")
+            {
+                var assemblies = await AssemblyLoader.LoadAssembliesAsync(new[] { "LazyTest.Client.Lazy.dll" });
+                lazyLoadedAssemblies.AddRange(assemblies);
+                Console.WriteLine($"loaded assemblies: {string.Join(",", assemblies.Select(i => i.FullName))}");
+            }
+        }
+
+        private List<Assembly> lazyLoadedAssemblies = new();
+    }
+}
